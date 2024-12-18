aa8e8c19cbec4f01504b0c2d388124d5b1b16c21
Peter van Lunteren <contact@pvanlunteren.com>
2024-12-18 14:18:18 +0100

    Update install-osx-arm.sh

--- shell/install-osx-arm.sh
+++ shell/install-osx-arm.sh
@@ -6,60 +6,62 @@
 
 ### refresh root
 root="$HOME/EcoAssist"
-if [ -d "EcoAssist" ]; then
-    rm -rf EcoAssist
-    echo "EcoAssist folder removed"
+if [ -d "${root}" ]; then
+    rm -rf "${root}"
+    echo "$root folder removed"
 else
-    echo "EcoAssist folder does not exist"
+    echo "$root folder does not exist"
 fi
 
 ### create folder structure
-mkdir -p EcoAssist
-mkdir -p EcoAssist/envs
-mkdir -p EcoAssist/models
-mkdir -p EcoAssist/models/det
-mkdir -p EcoAssist/models/cls
-mkdir -p "EcoAssist/models/det/MegaDetector 5a"
-mkdir -p EcoAssist/yolov5_versions/yolov5_old
-mkdir -p EcoAssist/yolov5_versions/yolov5_new
-echo "Hello world!" >> "EcoAssist/first-startup.txt"
+mkdir -p "${root}"
+mkdir -p "${root}/envs"
+mkdir -p "${root}/models"
+mkdir -p "${root}/models/det"
+mkdir -p "${root}/models/cls"
+mkdir -p "${root}/models/det/MegaDetector 5a"
+mkdir -p "${root}/yolov5_versions/yolov5_old"
+mkdir -p "${root}/yolov5_versions/yolov5_new"
+echo "Hello world!" >> "${root}/first-startup.txt"
 
 ## clone repositories
-git clone --depth 1 https://github.com/PetervanLunteren/EcoAssist-experiments.git "EcoAssist/EcoAssist-experiments"
-rm -rf "EcoAssist/EcoAssist-experiments/.git"
-mv "EcoAssist/EcoAssist-experiments" "EcoAssist/EcoAssist" # DEBUG
-mv "EcoAssist/EcoAssist/main.py" "EcoAssist/main.py"
+git clone --depth 1 https://github.com/PetervanLunteren/EcoAssist-experiments.git "${root}/EcoAssist-experiments"
+rm -rf "${root}/EcoAssist-experiments/.git"
+mv "${root}/EcoAssist-experiments" "${root}/EcoAssist" # DEBUG
+mv "${root}/EcoAssist/main.py" "${root}/main.py"
 echo "EcoAssist cloned"
 
-git clone https://github.com/agentmorris/MegaDetector.git "EcoAssist/MegaDetector"
-git -C "EcoAssist/MegaDetector" checkout e8a4fc19a2b9ad1892dd9ce65d437252df271576
-rm -rf "EcoAssist/MegaDetector/.git"
-mv "EcoAssist/MegaDetector" "EcoAssist/cameratraps"
+git clone https://github.com/agentmorris/MegaDetector.git "${root}/MegaDetector"
+git -C "${root}/MegaDetector" checkout e8a4fc19a2b9ad1892dd9ce65d437252df271576
+rm -rf "${root}/MegaDetector/.git"
+mv "${root}/MegaDetector" "${root}/cameratraps"
 echo "MegaDetector cloned"
 
-git clone https://github.com/ultralytics/yolov5.git "EcoAssist/yolov5_versions/yolov5_old/yolov5"
-git -C "EcoAssist/yolov5_versions/yolov5_old/yolov5" checkout 868c0e9bbb45b031e7bfd73c6d3983bcce07b9c1
-rm -rf "EcoAssist/yolov5_versions/yolov5_old/yolov5/.git"
+git clone https://github.com/ultralytics/yolov5.git "${root}/yolov5_versions/yolov5_old/yolov5"
+git -C "${root}/yolov5_versions/yolov5_old/yolov5" checkout 868c0e9bbb45b031e7bfd73c6d3983bcce07b9c1
+rm -rf "${root}/yolov5_versions/yolov5_old/yolov5/.git"
 echo "yolov5 old version cloned"
 
-git clone https://github.com/ultralytics/yolov5.git "EcoAssist/yolov5_versions/yolov5_new/yolov5"
-git -C "EcoAssist/yolov5_versions/yolov5_new/yolov5" checkout 3e55763d45f9c5f8217e4dad5ba1e6c1f42e3bf8
-rm -rf "EcoAssist/yolov5_versions/yolov5_new/yolov5/.git"
+git clone https://github.com/ultralytics/yolov5.git "${root}/yolov5_versions/yolov5_new/yolov5"
+git -C "${root}/yolov5_versions/yolov5_new/yolov5" checkout 3e55763d45f9c5f8217e4dad5ba1e6c1f42e3bf8
+rm -rf "${root}/yolov5_versions/yolov5_new/yolov5/.git"
 echo "yolov5 new version cloned"
 
-git clone --depth 1 https://github.com/PetervanLunteren/Human-in-the-loop.git "EcoAssist/Human-in-the-loop"
-rm -rf "EcoAssist/Human-in-the-loop/.git"
+git clone --depth 1 https://github.com/PetervanLunteren/Human-in-the-loop.git "${root}/Human-in-the-loop"
+rm -rf "${root}/Human-in-the-loop/.git"
 echo "Human-in-the-loop cloned"
 
-git clone --depth 1 https://github.com/PetervanLunteren/visualise_detection.git "EcoAssist/visualise_detection"
-rm -rf "EcoAssist/visualise_detection/.git"
+git clone --depth 1 https://github.com/PetervanLunteren/visualise_detection.git "${root}/visualise_detection"
+rm -rf "${root}/visualise_detection/.git"
 echo "visualise_detection cloned"
 
 ### download megadetector 
-curl -L https://github.com/agentmorris/MegaDetector/releases/download/v5.0/md_v5a.0.0.pt -o "EcoAssist/models/det/MegaDetector 5a/md_v5a.0.0.pt"
+curl -L https://github.com/agentmorris/MegaDetector/releases/download/v5.0/md_v5a.0.0.pt -o "${root}/models/det/MegaDetector 5a/md_v5a.0.0.pt"
 
 # print current working directory
 echo "Current working directory: $(pwd)"
+
+echo "root: ${root}"
 
 # print all files in current working directory
 echo "Files in current working directory:"
@@ -67,9 +69,7 @@ ls -la
 
 # print all files in EcoAssist folder
 echo "Files in EcoAssist folder:"
-ls -la EcoAssist
-
-exit 1
+ls -l ${root}
 
 ### source conda 
 if [ -f /Applications/.EcoAssist_files/miniforge/etc/profile.d/conda.sh ]; then
@@ -84,8 +84,8 @@ if [ -f "$HOME/miniforge/etc/profile.d/conda.sh" ]; then
 fi
 
 ### install env-base
-$conda_exe env create --file="EcoAssist/cameratraps/envs/environment-detector-m1.yml" -p "EcoAssist/envs/env-base"
-$conda_exe activate "EcoAssist/envs/env-base"
+$conda_exe env create --file="${root}/cameratraps/envs/environment-detector-m1.yml" -p "${root}/envs/env-base"
+$conda_exe activate "${root}/envs/env-base"
 $conda_exe install pytorch==1.13.1 torchvision==0.14.1 torchaudio==0.13.1 -c pytorch -y
 $conda_exe uninstall opencv -y
 pip install opencv-python
@@ -109,11 +109,11 @@ make Human-in-the-loop/pyside6
 $conda_exe deactivate
 
 ### install env-tensorflow
-$conda_exe env create --file="EcoAssist/EcoAssist/classification_utils/envs/tensorflow-macos-silicon.yml" -p "EcoAssist/envs/env-tensorflow"
+$conda_exe env create --file="${root}/EcoAssist/classification_utils/envs/tensorflow-macos-silicon.yml" -p "${root}/envs/env-tensorflow"
 
 ### install env-pytorch
-$conda_exe create -p "EcoAssist/envs/env-pytorch" python=3.8 -y
-$conda_exe activate "EcoAssist/envs/env-pytorch"
+$conda_exe create -p "${root}/envs/env-pytorch" python=3.8 -y
+$conda_exe activate "${root}/envs/env-pytorch"
 pip install torch==2.0.1 torchvision==0.15.2 torchaudio==2.0.2
 pip install "ultralytics==8.0.191"
 pip install "numpy==1.24.1"
@@ -124,8 +124,8 @@ pip install dill
 $conda_exe deactivate
 
 ### install env-pywildlife
-$conda_exe create -p "EcoAssist/envs/env-pywildlife" python=3.8 -y
-$conda_exe activate "EcoAssist/envs/env-pywildlife"
+$conda_exe create -p "${root}/envs/env-pywildlife" python=3.8 -y
+$conda_exe activate "${root}/envs/env-pywildlife"
 pip install pytorchwildlife
 pip install "setuptools<70"
 pip install jsonpickle
@@ -134,7 +134,7 @@ $conda_exe deactivate
 # create fresh pyinstaller environment
 $conda_exe create -n fresh python=3.8 pyinstaller -y
 $conda_exe activate fresh
-pyinstaller --onefile --windowed --icon="EcoAssist/EcoAssist/imgs/logo_small_bg.icns" "EcoAssist/main.py"
+pyinstaller --onefile --windowed --icon="${root}/EcoAssist/imgs/logo_small_bg.icns" --distpath="${HOME}/dist" --workpath="${HOME}/build" "${root}/main.py"
 $conda_exe deactivate
 
 # clean
@@ -142,5 +142,5 @@ $conda_exe clean --all --yes --force-pkgs-dirs
 $conda_exe clean --all --yes
 
 # move and rename executables to EcoAssist
-mv "dist/main" "EcoAssist/debug_mode"
-mv "dist/main.app" "EcoAssist/EcoAssist ${RELEASE_VERSION}.app"
+mv "${HOME}/dist/main" "${root}/debug_mode"
+mv "${HOME}/dist/main.app" "${root}/EcoAssist ${RELEASE_VERSION}.app"
