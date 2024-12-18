bb715957a51e544281151bfaa6f600bc13328fe0
Peter van Lunteren <contact@pvanlunteren.com>
2024-12-18 10:22:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -20,9 +20,10 @@ def show_info_message(message):
     else:
         print("Unsupported OS")
         return
-show_info_message("Starting EcoAssist...\n\n"
-                  "This may take a few minutes initially as dependencies, environments, and models are loaded.\n\n"
-                  "Don't worry – subsequent starts will be faster!")
+
+# os dependent python executables
+def get_python_interprator(env_name):
+    return os.path.join(EcoAssist_files, "envs", f"env-{env_name}", "bin", "python")
 
 # clean path
 if getattr(sys, 'frozen', False):
@@ -35,16 +36,22 @@ if EcoAssist_files.endswith("main.app/Contents/MacOS"):
     
 if EcoAssist_files.endswith(".app/Contents/MacOS"):
     EcoAssist_files = os.path.dirname(os.path.dirname(os.path.dirname(EcoAssist_files)))
-    
-GUI_script = os.path.join(EcoAssist_files, "EcoAssist", "EcoAssist_GUI.py")
 
+# init paths    
+GUI_script = os.path.join(EcoAssist_files, "EcoAssist", "EcoAssist_GUI.py")
+first_startup_file = os.path.join(EcoAssist_files, "first_startup.txt")
+
+# show info message
+if not os.path.exists(first_startup_file):
+    show_info_message("Starting EcoAssist...\n\n"
+                    "This may take a few minutes initially as dependencies, environments, and models are loaded.\n\n"
+                    "Don't worry – subsequent starts will be faster!")
+
+# log
 print(f"  EcoAssist_files: {EcoAssist_files}")
 print(f"   sys.executable: {sys.executable}")
 print(f"       GUI_script: {GUI_script}")
 
-# os dependent python executables
-def get_python_interprator(env_name):
-    return os.path.join(EcoAssist_files, "envs", f"env-{env_name}", "bin", "python")
-
 # run the GUI script
+print("Running GUI script...")
 subprocess.run([get_python_interprator("base"), GUI_script])
