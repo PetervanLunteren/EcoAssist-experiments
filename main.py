39ab8855cc6107edb08236712bdf39c4a0e903bf
Peter van Lunteren <contact@pvanlunteren.com>
2024-12-18 09:58:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,8 @@
 import os
 import subprocess
 import sys
-import PySimpleGUI as sg
 
-# clean ecoassist fpath
+# clean path
 if getattr(sys, 'frozen', False):
     EcoAssist_files = os.path.dirname(sys.executable)
 else:
@@ -20,24 +19,15 @@ if EcoAssist_files.endswith("main.app/Contents/MacOS"):
 if EcoAssist_files.endswith(".app/Contents/MacOS"):
     EcoAssist_files = os.path.dirname(os.path.dirname(os.path.dirname(EcoAssist_files)))
     
-# script path
 GUI_script = os.path.join(EcoAssist_files, "EcoAssist", "EcoAssist_GUI.py")
 
-# open simple window
-first_startup_file = os.path.join(EcoAssist_files, "first-startup.txt")
-if os.path.isfile(first_startup_file):
-    layout = [[sg.Text("Starting up EcoAssist...\n"
-                       "This may take a few minutes the first time due to the initial loading of all the dependencies, conda environments, and models.\n"
-                       "All subsequent times will be faster.")]]
-    window = sg.Window("Progress", layout)
+print(f"  EcoAssist_files: {EcoAssist_files}")
+print(f"   sys.executable: {sys.executable}")
+print(f"       GUI_script: {GUI_script}")
 
-# fetch python interprator
+# os dependent python executables
 def get_python_interprator(env_name):
     return os.path.join(EcoAssist_files, "envs", f"env-{env_name}", "bin", "python")
 
-# check base env
+# run the GUI script
 subprocess.run([get_python_interprator("base"), GUI_script])
-
-# close window
-if 'window' in locals():
-    window.close()
