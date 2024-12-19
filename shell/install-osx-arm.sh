958c8ad9477a1fbe85a57f4904a69ddc2f50963b
Peter van Lunteren <contact@pvanlunteren.com>
2024-12-19 10:58:27 +0100

    Update install-osx-arm.sh

--- shell/install-osx-arm.sh
+++ shell/install-osx-arm.sh
@@ -31,32 +31,32 @@ mv "${root}/EcoAssist-experiments" "${root}/EcoAssist" # DEBUG
 mv "${root}/EcoAssist/main.py" "${root}/main.py"
 echo "EcoAssist cloned"
 
-git clone https://github.com/agentmorris/MegaDetector.git "${root}/MegaDetector"
-git -C "${root}/MegaDetector" checkout e8a4fc19a2b9ad1892dd9ce65d437252df271576
-rm -rf "${root}/MegaDetector/.git"
-mv "${root}/MegaDetector" "${root}/cameratraps"
-echo "MegaDetector cloned"
+# git clone https://github.com/agentmorris/MegaDetector.git "${root}/MegaDetector"
+# git -C "${root}/MegaDetector" checkout e8a4fc19a2b9ad1892dd9ce65d437252df271576
+# rm -rf "${root}/MegaDetector/.git"
+# mv "${root}/MegaDetector" "${root}/cameratraps"
+# echo "MegaDetector cloned"
 
-git clone https://github.com/ultralytics/yolov5.git "${root}/yolov5_versions/yolov5_old/yolov5"
-git -C "${root}/yolov5_versions/yolov5_old/yolov5" checkout 868c0e9bbb45b031e7bfd73c6d3983bcce07b9c1
-rm -rf "${root}/yolov5_versions/yolov5_old/yolov5/.git"
-echo "yolov5 old version cloned"
+# git clone https://github.com/ultralytics/yolov5.git "${root}/yolov5_versions/yolov5_old/yolov5"
+# git -C "${root}/yolov5_versions/yolov5_old/yolov5" checkout 868c0e9bbb45b031e7bfd73c6d3983bcce07b9c1
+# rm -rf "${root}/yolov5_versions/yolov5_old/yolov5/.git"
+# echo "yolov5 old version cloned"
 
-git clone https://github.com/ultralytics/yolov5.git "${root}/yolov5_versions/yolov5_new/yolov5"
-git -C "${root}/yolov5_versions/yolov5_new/yolov5" checkout 3e55763d45f9c5f8217e4dad5ba1e6c1f42e3bf8
-rm -rf "${root}/yolov5_versions/yolov5_new/yolov5/.git"
-echo "yolov5 new version cloned"
+# git clone https://github.com/ultralytics/yolov5.git "${root}/yolov5_versions/yolov5_new/yolov5"
+# git -C "${root}/yolov5_versions/yolov5_new/yolov5" checkout 3e55763d45f9c5f8217e4dad5ba1e6c1f42e3bf8
+# rm -rf "${root}/yolov5_versions/yolov5_new/yolov5/.git"
+# echo "yolov5 new version cloned"
 
-git clone --depth 1 https://github.com/PetervanLunteren/Human-in-the-loop.git "${root}/Human-in-the-loop"
-rm -rf "${root}/Human-in-the-loop/.git"
-echo "Human-in-the-loop cloned"
+# git clone --depth 1 https://github.com/PetervanLunteren/Human-in-the-loop.git "${root}/Human-in-the-loop"
+# rm -rf "${root}/Human-in-the-loop/.git"
+# echo "Human-in-the-loop cloned"
 
-git clone --depth 1 https://github.com/PetervanLunteren/visualise_detection.git "${root}/visualise_detection"
-rm -rf "${root}/visualise_detection/.git"
-echo "visualise_detection cloned"
+# git clone --depth 1 https://github.com/PetervanLunteren/visualise_detection.git "${root}/visualise_detection"
+# rm -rf "${root}/visualise_detection/.git"
+# echo "visualise_detection cloned"
 
-### download megadetector 
-curl -L https://github.com/agentmorris/MegaDetector/releases/download/v5.0/md_v5a.0.0.pt -o "${root}/models/det/MegaDetector 5a/md_v5a.0.0.pt"
+# ### download megadetector 
+# curl -L https://github.com/agentmorris/MegaDetector/releases/download/v5.0/md_v5a.0.0.pt -o "${root}/models/det/MegaDetector 5a/md_v5a.0.0.pt"
 
 ### source conda 
 if [ -f "/Applications/.EcoAssist_files/miniforge/etc/profile.d/conda.sh" ]; then
@@ -72,47 +72,53 @@ if [ -f "$HOME/miniforge/etc/profile.d/conda.sh" ]; then
     echo "Conda found in $HOME/miniforge"
 fi
 
-### install env-base
-$conda_exe env create --file="${root}/cameratraps/envs/environment-detector-m1.yml" -p "${root}/envs/env-base"
-$conda_exe run -p "${root}/envs/env-base" conda install pytorch==1.13.1 torchvision==0.14.1 torchaudio==0.13.1 -c pytorch -y
-$conda_exe run -p "${root}/envs/env-base" conda uninstall opencv -y
-$conda_exe run -p "${root}/envs/env-base" pip install opencv-python
-$conda_exe run -p "${root}/envs/env-base" pip install RangeSlider
-$conda_exe run -p "${root}/envs/env-base" pip install gpsphoto
-$conda_exe run -p "${root}/envs/env-base" pip install exifread
-$conda_exe run -p "${root}/envs/env-base" pip install piexif
-$conda_exe run -p "${root}/envs/env-base" pip install openpyxl
-$conda_exe run -p "${root}/envs/env-base" pip install customtkinter
-$conda_exe run -p "${root}/envs/env-base" pip install CTkTable
-$conda_exe run -p "${root}/envs/env-base" pip install folium
-$conda_exe run -p "${root}/envs/env-base" pip install plotly
-$conda_exe run -p "${root}/envs/env-base" pip install "gitpython>=3.1.30"
-$conda_exe run -p "${root}/envs/env-base" pip install "tensorboard>=2.4.1"
-$conda_exe run -p "${root}/envs/env-base" pip install "thop>=0.1.1"
-$conda_exe run -p "${root}/envs/env-base" pip install "protobuf<=3.20.1"
-$conda_exe run -p "${root}/envs/env-base" pip install "setuptools>=65.5.1"
-$conda_exe run -p "${root}/envs/env-base" pip install PySide6
-$conda_exe run -p "${root}/envs/env-base" conda install lxml -y
-make "${root}/Human-in-the-loop/pyside6"
+# DEBUG
+$conda_exe create -p "${root}/envs/env-debug" python=3.8 -y
 
-### install env-tensorflow
-$conda_exe env create --file="${root}/EcoAssist/classification_utils/envs/tensorflow-macos-silicon.yml" -p "${root}/envs/env-tensorflow"
+# ### install env-base
+# $conda_exe env create --file="${root}/cameratraps/envs/environment-detector-m1.yml" -p "${root}/envs/env-base"
+# $conda_exe run -p "${root}/envs/env-base" conda install pytorch==1.13.1 torchvision==0.14.1 torchaudio==0.13.1 -c pytorch -y
+# $conda_exe run -p "${root}/envs/env-base" conda uninstall opencv -y
+# $conda_exe run -p "${root}/envs/env-base" pip install opencv-python
+# $conda_exe run -p "${root}/envs/env-base" pip install RangeSlider
+# $conda_exe run -p "${root}/envs/env-base" pip install gpsphoto
+# $conda_exe run -p "${root}/envs/env-base" pip install exifread
+# $conda_exe run -p "${root}/envs/env-base" pip install piexif
+# $conda_exe run -p "${root}/envs/env-base" pip install openpyxl
+# $conda_exe run -p "${root}/envs/env-base" pip install customtkinter
+# $conda_exe run -p "${root}/envs/env-base" pip install CTkTable
+# $conda_exe run -p "${root}/envs/env-base" pip install folium
+# $conda_exe run -p "${root}/envs/env-base" pip install plotly
+# $conda_exe run -p "${root}/envs/env-base" pip install "gitpython>=3.1.30"
+# $conda_exe run -p "${root}/envs/env-base" pip install "tensorboard>=2.4.1"
+# $conda_exe run -p "${root}/envs/env-base" pip install "thop>=0.1.1"
+# $conda_exe run -p "${root}/envs/env-base" pip install "protobuf<=3.20.1"
+# $conda_exe run -p "${root}/envs/env-base" pip install "setuptools>=65.5.1"
+# $conda_exe run -p "${root}/envs/env-base" pip install PySide6
+# $conda_exe run -p "${root}/envs/env-base" conda install lxml -y
+# make "${root}/Human-in-the-loop/pyside6"
 
-### install env-pytorch
-$conda_exe create -p "${root}/envs/env-pytorch" python=3.8 -y
-$conda_exe run -p "${root}/envs/env-pytorch" pip install torch==2.0.1 torchvision==0.15.2 torchaudio==2.0.2
-$conda_exe run -p "${root}/envs/env-pytorch" pip install "ultralytics==8.0.191"
-$conda_exe run -p "${root}/envs/env-pytorch" pip install "numpy==1.24.1"
-$conda_exe run -p "${root}/envs/env-pytorch" pip install "humanfriendly==10.0"
-$conda_exe run -p "${root}/envs/env-pytorch" pip install "jsonpickle==3.0.2"
-$conda_exe run -p "${root}/envs/env-pytorch" pip install timm
-$conda_exe run -p "${root}/envs/env-pytorch" pip install dill
+# ### install env-tensorflow
+# $conda_exe env create --file="${root}/EcoAssist/classification_utils/envs/tensorflow-macos-silicon.yml" -p "${root}/envs/env-tensorflow"
 
-### install env-pywildlife
-$conda_exe create -p "${root}/envs/env-pywildlife" python=3.8 -y
-$conda_exe run -p "${root}/envs/env-pywildlife" pip install pytorchwildlife
-$conda_exe run -p "${root}/envs/env-pywildlife" pip install "setuptools<70"
-$conda_exe run -p "${root}/envs/env-pywildlife" pip install jsonpickle
+# ### install env-pytorch
+# $conda_exe create -p "${root}/envs/env-pytorch" python=3.8 -y
+# $conda_exe run -p "${root}/envs/env-pytorch" pip install torch==2.0.1 torchvision==0.15.2 torchaudio==2.0.2
+# $conda_exe run -p "${root}/envs/env-pytorch" pip install "ultralytics==8.0.191"
+# $conda_exe run -p "${root}/envs/env-pytorch" pip install "numpy==1.24.1"
+# $conda_exe run -p "${root}/envs/env-pytorch" pip install "humanfriendly==10.0"
+# $conda_exe run -p "${root}/envs/env-pytorch" pip install "jsonpickle==3.0.2"
+# $conda_exe run -p "${root}/envs/env-pytorch" pip install timm
+# $conda_exe run -p "${root}/envs/env-pytorch" pip install dill
+
+# ### install env-pywildlife
+# $conda_exe create -p "${root}/envs/env-pywildlife" python=3.8 -y
+# $conda_exe run -p "${root}/envs/env-pywildlife" pip install pytorchwildlife
+# $conda_exe run -p "${root}/envs/env-pywildlife" pip install "setuptools<70"
+# $conda_exe run -p "${root}/envs/env-pywildlife" pip install jsonpickle
+
+### rename folder
+mv "${root}" "$HOME/EcoAssist ${RELEASE_VERSION}"
 
 # # create fresh pyinstaller environment
 # $conda_exe create -n fresh python=3.8 pyinstaller -y
